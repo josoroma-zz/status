952f20ea77e97958ced33f43a0c6a5e18c89df52
porozco <porozco@fh.org>
2016-08-12 17:03:59 -0600

    <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>

--- src/components/statusApp.js
+++ src/components/statusApp.js
@@ -13,9 +13,9 @@ import { ALL_STATUSES, PUBLIC_STATUSES, FRIEND_STATUSES } from '../constants';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import {green100, green500, green700} from 'material-ui/styles/colors';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import AppBar from 'material-ui/AppBar';
 
 @observer
@@ -23,20 +23,8 @@ export default class StatusApp extends React.Component {
 	render() {
 		const {statusStore, viewStore} = this.props;
 
-        const muiTheme = getMuiTheme({
-          palette: {
-            primary1Color: green500,
-            primary2Color: green700,
-            primary3Color: green100,
-          },
-        }, {
-          avatar: {
-            borderColor: null,
-          }
-        });
-
 		return (
-            <MuiThemeProvider muiTheme={muiTheme}>
+            <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
     			<div className="container">
                     <AppBar title="Josoroma" />
                     <StatusHeader statusStore={statusStore} viewStore={viewStore} />
